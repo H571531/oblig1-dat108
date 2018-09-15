3417a559da3ab1320a874847ba225fa5958f1e8b
tohov <tohov@DESKTOP-A7L6L26>
2018-09-15 13:34:37 +0200

    no.hvl.dat108.oppgave2_WP

---
+++ Oblig1-Dat108/src/no/hvl/dat108/oppgave2_WP/Klient.java
@@ -0,0 +1,21 @@
+package no.hvl.dat108.oppgave2_WP;
+
+public class Klient {
+
+	public static void main(String[] args) {
+		
+		Buffer buffer = new Buffer();
+		
+		Thread prod = new Producer(buffer);
+		Thread prod2 = new Producer(buffer);
+		Thread cons = new Consumer(buffer);
+		Thread cons2 = new Consumer(buffer);
+		
+		prod.start();
+		prod2.start();
+		cons.start();
+		cons2.start();
+
+	}
+
+}
